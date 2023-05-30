db37b8cbf7d30945142940c4994a2d42d89e33cd
jakob-melendez <jakobmelendez001@gmail.com>
2023-05-30 13:12:05 -0400

    Setting up structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <NavBar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path="*" element={<Navigate to="/"/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+    <h1>About</h1>
+}
+
+export default About;
